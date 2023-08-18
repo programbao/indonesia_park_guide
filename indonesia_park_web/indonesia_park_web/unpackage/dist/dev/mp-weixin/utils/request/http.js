9ce612538017b2beaa37a1f90e89749d9c5f1785
xiebaozhong <xiexiaobaozj@163.com>
2023-08-18 17:15:02 +0900

    feat: 调整目录结构

--- indonesia_park_web/indonesia_park_web/unpackage/dist/dev/mp-weixin/utils/request/http.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const utils_request_request = require("./request.js");
-function apiGet(url, params, token) {
-  return utils_request_request.request(url, params, "GET", token);
-}
-exports.apiGet = apiGet;
